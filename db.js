447f76947f9be62226e96a0097d0c154c1b793b6
Zeeshan Ahmad <99203352+Zeeshan8064@users.noreply.github.com>
2024-12-27 01:53:18 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-console.log('Connecting to database...');
-console.log('Mongoose version:', mongoose.version);
-console.log('MONGO_URL:', process.env.MONGO_URL);
-console.log('DB_NAME:', process.env.DB_NAME);
-
-
-mongoose.connect('mongodb+srv://Zeeshanahmad8064:ZA101639@cluster0.kuy7b.mongodb.net/fitnessgeek?retryWrites=true&w=majority', {
-  dbName: 'fitnessgeek',
-  serverSelectionTimeoutMS: 60000, // Increase timeout for debugging
+mongoose.connect(process.env.MONGO_URL,{
+    dbName: process.env.DB_NAME
+}).then(
+    () => {
+        console.log('Connected to database');
+    }
+).catch((err) => {
+    console.log('Error connecting to database ' + err);
 })
-  .then(() => {
-    console.log('Connected to database');
-  })
-  .catch((err) => {
-    console.error('Error connecting to database:', err.message);
-    console.error(err); // Log the entire error object for debugging
-  });
