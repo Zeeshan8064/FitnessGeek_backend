16f9f6de08f7cc86fc2c2714c7a22334064cd4ca
Zeeshan Ahmad <99203352+Zeeshan8064@users.noreply.github.com>
2024-12-27 17:49:17 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,37 +1,21 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-console.log('Attempting MongoDB connection...');
+const uri = process.env.MONGO_URL;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: process.env.DB_NAME,
+  serverSelectionTimeoutMS: 10000000, // 10000 seconds
+};
 
-mongoose.set('debug', true);
-
-(async () => {
-  try {
-    const options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: process.env.DB_NAME,
-      maxPoolSize: 10,
-      serverApi: {
-        version: '1',
-        strict: true,
-        deprecationErrors: true
-      }
-    };
-
-    console.log('Connection URL:', process.env.MONGO_URL?.split('@')[1]);
-    await mongoose.connect(process.env.MONGO_URL, options);
-    
-    mongoose.connection.on('connected', () => {
-      console.log('Mongoose connected');
-    });
-
-    mongoose.connection.on('error', (err) => {
-      console.error('Mongoose error:', err);
-    });
-
-  } catch (err) {
-    console.error('Connection failed:', err.message);
+console.log('Testing MongoDB connection...');
+mongoose.connect(uri, options)
+  .then(() => {
+    console.log('MongoDB connection successful!');
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error('MongoDB connection failed:', err.message);
     process.exit(1);
-  }
-})();
+  });
