a2ee11e00fed4ba1bb4f1f380012385bb9c4647b
Zeeshan Ahmad <99203352+Zeeshan8064@users.noreply.github.com>
2024-12-27 01:50:15 +0100

    Update db.js

--- db.js
+++ db.js
@@ -7,8 +7,8 @@ console.log('MONGO_URL:', process.env.MONGO_URL);
 console.log('DB_NAME:', process.env.DB_NAME);
 
 
-mongoose.connect(process.env.MONGO_URL, {
-  dbName: process.env.DB_NAME,
+mongoose.connect('mongodb+srv://Zeeshanahmad8064:ZA101639@cluster0.kuy7b.mongodb.net/fitnessgeek?retryWrites=true&w=majority', {
+  dbName: 'fitnessgeek',
   serverSelectionTimeoutMS: 60000, // Increase timeout for debugging
 })
   .then(() => {
