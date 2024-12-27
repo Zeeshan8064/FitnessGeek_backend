d9773f51d2ce39f5f11260b8418d58539fc7d05b
Zeeshan Ahmad <99203352+Zeeshan8064@users.noreply.github.com>
2024-12-27 18:15:48 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,22 +1,26 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
 
-const uri = 'mongodb://52.58.203.31:27017/fitnessgeek';
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://zeeshanahmad8064:ZA101639@cluster1.jzeeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: process.env.DB_NAME,
-  serverSelectionTimeoutMS: 10000000, // 10000 seconds
-};
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-console.log('Testing MongoDB connection...');
-mongoose.connect(uri, options)
-  .then(() => {
-    console.log('MongoDB connection successful!');
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error('MongoDB connection failed:', err.message);
-    process.exit(1);
-  });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
