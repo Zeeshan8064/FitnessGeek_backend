8db4aaeb081be5f8267b57454479a27e0c14a530
Zeeshan Ahmad <99203352+Zeeshan8064@users.noreply.github.com>
2024-12-28 23:12:36 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,46 +1,15 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 require('dotenv').config();
 
-const uri = process.env.MONGO_URL;
+console.log('Connecting to database..');
 
-// Enable Mongoose debugging in development
-mongoose.set('debug', true);
 
-const connectToDatabase = async () => {
-  try {
-    console.log('Attempting MongoDB connection...');
-    
-    // Use Mongoose to connect to MongoDB
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
-
-    console.log("Successfully connected to MongoDB!");
-
-    // Add a listener for the Mongoose connection
-    mongoose.connection.on('connected', () => {
-      console.log('Mongoose is connected to the database');
-    });
-
-    mongoose.connection.on('error', (err) => {
-      console.error('Mongoose connection error:', err.message);
-    });
-
-    mongoose.connection.on('disconnected', () => {
-      console.warn('Mongoose connection disconnected');
-    });
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err.message);
-    process.exit(1); // Exit the process with failure
-  }
-};
-
-// Export the connection function for use in your application
-module.exports = connectToDatabase;
+mongoose.connect(process.env.MONGO_URL,{
+    dbName: process.env.DB_NAME
+}).then(
+    () => {
+        console.log('Connected to database');
+    }
+).catch((err) => {
+    console.log('Error connecting to database ' + err);
+})
